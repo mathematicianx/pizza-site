c7d321544b376027e4cd0381e251c04f1d77505f
Tomasz Solarewicz <tomasz.solarewicz@gmail.com>
2021-02-14 11:32:06 +0100

    code refactorization, added basic dropdown buttons with options, created basic functionality for salads and drinks

--- src/index.js
+++ src/index.js
@@ -2,14 +2,31 @@ import React from "react";
 import ReactDom from "react-dom";
 import Button from "react-bootstrap/Button";
 
+// FONTS AWESOME ICONS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhone,
+  faEnvelope,
+  faClock,
+  faMapMarkerAlt,
+} from "@fortawesome/free-solid-svg-icons";
+
 // MEDIA
 import specific_logo from "./images/chad-montano-MqT0asuoIcU-unsplash.jpg";
+import horizontal_logo from "./images/pizza-seeklogo.com_horizontal_bar.png";
+import sauce_photo from "./images/pizza_sauce.png";
+import salad_photo from "./images/salad_photo.jpg";
+import drink_photo from "./images/drink_photo.jpg";
 
 // CSS
 import "./index.css";
 
 // BOOTSTRAP CSS
 import "bootstrap/dist/css/bootstrap.min.css";
+import Popper from "popper.js";
+import "jquery/dist/jquery.js";
+
+import "bootstrap/dist/js/bootstrap.js";
 
 function TopBar() {
   console.log("topbar");
@@ -25,6 +42,8 @@ function ProductList() {
     <div>
       <PizzaList />
       <Extras />
+      <Salads />
+      <Drinks />
     </div>
   );
 }
@@ -33,11 +52,11 @@ function PizzaList() {
   console.log("test");
   return (
     <section className="pizzalist">
-      <OnePizzaRow name="1.Margherita" />
-      <OnePizzaRow name="2.Hawaii" />
-      <OnePizzaRow name="3.Prosciutto" />
-      <OnePizzaRow name="4.Vegetariana" />
-      <OnePizzaRow name="5.Siciliana" />
+      <OneRow name="1.Margherita" id="pizza" />
+      <OneRow name="2.Hawaii" id="pizza" />
+      <OneRow name="3.Prosciutto" id="pizza" />
+      <OneRow name="4.Vegetariana" id="pizza" />
+      <OneRow name="5.Siciliana" id="pizza" />
     </section>
   );
 }
@@ -46,9 +65,32 @@ function Extras() {
   return (
     <section className="pizzalist">
       <p>Lista dodatków</p>
-      <OnePizzaRow name="Sos czosnkowy" />
-      <OnePizzaRow name="Sos pomidorowy" />
-      <OnePizzaRow name="Sos Tysiąca Wysp" />
+      <OneRow name="Sos czosnkowy" id="sauce" />
+      <OneRow name="Sos pomidorowy" id="sauce" />
+      <OneRow name="Sos Tysiąca Wysp" id="sauce" />
+    </section>
+  );
+}
+
+function Salads() {
+  return (
+    <section className="pizzalist">
+      <p>Lista dodatków</p>
+      <OneRow name="Sałatka Gyros" id="salad" />
+      <OneRow name="Sałatka grecka" id="salad" />
+      <OneRow name="Sałatka Cezar" id="salad" />
+    </section>
+  );
+}
+
+function Drinks() {
+  return (
+    <section className="pizzalist">
+      <p>Lista dodatków</p>
+      <OneRow name="Woda" id="drink" />
+      <OneRow name="Cola 0,2 l" id="drink" />
+      <OneRow name="Cola 0,5 l" id="drink" />
+      <OneRow name="Lemoniada 0,2 l" id="drink" />
     </section>
   );
 }
@@ -57,9 +99,26 @@ function LeftMenu() {
   return (
     <section className="leftmenu">
       <div className="leftrow">
-        <p>Pizzeria</p>
-        <p>telefon: 123 456 789</p>
-        <p>email: test@test.com</p>
+        <p>
+          <img src={horizontal_logo} className="horizontal-logo"></img>
+        </p>
+        <p>
+          <FontAwesomeIcon icon={faPhone} />: +48 123 456 789
+        </p>
+        <p>
+          <FontAwesomeIcon icon={faEnvelope} />: pizza@site.com
+        </p>
+        <section>
+          <FontAwesomeIcon icon={faClock} /> Godziny otwarcia:
+          <ul>
+            <li>Pon-Pt: : 10:00 - 20:00</li>
+            <li>Sobota: 10:00 - 23:00</li>
+            <li>Niedziela: Nieczynne</li>
+          </ul>
+        </section>
+        <p>
+          <FontAwesomeIcon icon={faMapMarkerAlt} /> Szkolna 10, Siechnice
+        </p>
       </div>
       <hr></hr>
       <div className="leftrow">
@@ -97,38 +156,92 @@ const ColoredLine = ({ color }) => (
   />
 );
 
-function OnePizzaRow(props) {
+function WhichPhotoToServe(props) {
+  const id = props.id;
+  if (id === "pizza") {
+    return <img className="specific-photo" src={specific_logo}></img>;
+  } else if (id === "sauce") {
+    return <img className="specific-photo" src={sauce_photo}></img>;
+  } else if (id === "salad") {
+    return <img className="specific-photo" src={salad_photo}></img>;
+  } else if (id === "drink") {
+    return <img className="specific-photo" src={drink_photo}></img>;
+  }
+}
+
+function DropdownPizzaSizeSelection() {
   return (
-    <div>
-      <ColoredLine color="#D3D3D3" />
-      <div className="one-pizza">
-        <PizzaSpecificPhoto />
-        <h1 style={{ height: "100%" }}>{props.name}</h1>
-        <button variant="outline-primary" className="btn-primary">
-          Primary
-        </button>
-        <button variant="outline-secondary" className="btn-secondary">
-          Secondary
-        </button>
+    <div class="dropdown">
+      <button
+        class="btn btn-secondary dropdown-toggle"
+        type="button"
+        id="dropdownMenuButton"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
+        Wybierz rozmiar pizzy
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="#">
+          35 cm - 20 zł
+        </a>
+        <a class="dropdown-item" href="#">
+          45 cm - 28 zł
+        </a>
       </div>
     </div>
   );
 }
 
-function PizzaSpecificPhoto() {
-  return <img className="specific-photo" src={specific_logo}></img>;
+function DropdownPizzaQuantity() {
+  return (
+    <div class="dropdown">
+      <button
+        class="btn btn-secondary dropdown-toggle"
+        type="button"
+        id="dropdownMenuButton"
+        data-toggle="dropdown"
+        aria-haspopup="true"
+        aria-expanded="false"
+      >
+        Ilość
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="#">
+          1
+        </a>
+        <a class="dropdown-item" href="#">
+          2
+        </a>
+        <a class="dropdown-item" href="#">
+          3
+        </a>
+        <a class="dropdown-item" href="#">
+          4
+        </a>
+        <a class="dropdown-item" href="#">
+          5
+        </a>
+      </div>
+    </div>
+  );
 }
 
-// function BookList() {
-//   console.log(greeting);
-//   return (
-//     <section className="booklist">
-//       {data.map((book, index) => {
-//         return <SpecificBook key={book.id} {...book}></SpecificBook>;
-//       })}
-//     </section>
-//   );
-// }
+function OneRow(props) {
+  const id = props.id;
+  return (
+    <div>
+      <ColoredLine color="#D3D3D3" />
+      <div className="one-pizza">
+        <WhichPhotoToServe id={id} />
+        <h1 style={{ height: "100%" }}>{props.name}</h1>
+        <DropdownPizzaSizeSelection />
+        <DropdownPizzaQuantity />
+      </div>
+    </div>
+  );
+}
 
 function Main() {
   return (
